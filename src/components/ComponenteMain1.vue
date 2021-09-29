4c40c2b7178cb17c72a04cb81d29b76d347de5f7
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-29 17:24:56 +0200

    Aggiunta componente1 (di Main)

---
+++ src/components/ComponenteMain1.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="componente1">
+        <div class="contenitore-centrale">
+            <h2> --> Contents goes here </h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+.componente1{
+    background-color: #1C1C1C;
+
+    .contenitore-centrale{
+    max-width: 70%;
+    margin: auto;
+    height: 150px;
+    display: flex;
+    align-items: center;
+ 
+        h2{
+            color: white;
+        }
+    }
+}
+
+
+</style>
