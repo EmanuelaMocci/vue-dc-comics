91970b93cc726f61f31adcd681a9b4809b5717c2
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-29 18:15:30 +0200

    Aggiunta sezione componenteMain2 e footer

---
+++ src/components/ComponenteMain2.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="componente2">
+        <div class="contenitore-centrale">
+            <div v-for="(item, index) in menu" :key="index">
+                <img src="" alt="">
+            </div>
+        </div>
+     
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+    .componente2{
+        background-color: #0082F9;
+        height: 150px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        
+
+        .contenitore-centrale{
+            display: flex;
+            max-width: 70%;
+            margin: auto;
+
+            
+            .img{
+                display: flex;
+                align-items: center;
+
+                img{
+                    height: 50px;
+                }
+                span{
+                    color: white;
+                    padding-left: 10px;
+                }
+            }
+            
+            
+        }
+    }
+
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        <h2>Footer</h2>
+    <div class="footer">
+        <img src="@/assets/img/footer-bg.jpg" alt="sfondo footer">
+        <div class="contenitore-centrale"></div>
     </div>
+    
 </template>
 
 <script>
@@ -10,6 +12,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.footer{
+    img{
+        width: 100%;
+        height: 500px;
+    }
+}
+.contenitore-centrale{
+    max-width: 70%;
+    margin: auto;
+}
 
 </style>
