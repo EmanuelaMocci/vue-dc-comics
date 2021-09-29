6fb6d755d08b5b1a7b5b1216a08d48cf0aafd50e
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-29 20:50:37 +0200

    Aggiunta lista footer top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,56 @@
 <template>
     <div class="footer">
         <div class="footer-top">
-             <div class="contenitore-centrale"></div>
+             <div class="contenitore-centrale">
+                 <div class="lista">
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+
+                </div>
+                
+                <div class="lista">
+                    <h4>DC</h4>
+                    <ul>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+
+                <div class="lista">
+                    <h4>SITES</h4>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+                    
+             </div>
         </div>
         <div class="footer-bottom">
 
@@ -19,13 +68,36 @@ export default {
 
 <style lang="scss" scoped>
 .footer-top{
-    height: 300px;
+    height: 330px;
     width: 100%;
     background-image: url("../assets/img/footer-bg.jpg");
 
     .contenitore-centrale{
         max-width: 70%;
         margin: auto;
+        display: flex;
+
+        .lista{
+            padding-right: 30px;
+
+            h4{
+                color: white;
+            }
+            ul{
+                padding: 0;
+
+                li{
+                    list-style: none;
+
+                    a{
+                        color: grey;
+                        text-decoration: none;
+                        font-size: 12px;
+                        letter-spacing: 0.7px;
+                    }
+                }
+                }
+        }
     }
 }
 
