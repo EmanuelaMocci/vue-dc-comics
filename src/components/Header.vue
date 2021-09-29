614a6fe9884933de1c8790b12849667d57cc6bd2
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-29 16:45:08 +0200

    Aggiunta header con menu non dinamico

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,55 @@
 <template>
-    <div class="header-contenitore">
-        <h1>Header</h1>
+    <div class="header">
+        <div class="logo">
+            <img src="@/assets/img/dc-logo.png" alt="logo">
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Header'
-    
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.header{
+    max-width: 70%;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 0;
 
+    ul{
+        display: flex;
+        
+        li{
+            padding-left: 30px;
+            list-style: none;
+            
+            a{
+                color: black;
+                font-size: 13px;
+                text-decoration: none;
+                font-weight: bold;
+                letter-spacing: 0.7px;
+            }
+        }
+    }
+}
 </style>
