1555e333e189623f75b3657f31ed9b89aa434bc0
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-30 18:04:58 +0200

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Hero/>
+    <main>
+       <componenteMain1/>
+       <componenteMain2/>
+    </main>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Hero from './components/Hero.vue';
+import componenteMain1 from './components/ComponenteMain1.vue';
+import componenteMain2 from './components/ComponenteMain2.vue';
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Hero,
+    componenteMain1,
+    componenteMain2,
+    Footer
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  font-family: 'PT Sans Narrow', sans-serif;
 }
 </style>
